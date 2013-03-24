b2d0aa0091c7abf30bc6e8cfa1ef6daeb6e3790c
Mateusz Cieciura <mateusz5000@gmail.com>
2013-03-24 21:25:23 +0100

    Entropy function

--- lab5/entropy.py
+++ lab5/entropy.py
@@ -1,24 +1,26 @@
-import sys, re, string, operator
+import sys, re, string
 from math import log
 
-stat = {}
+def calculate_entropy():
+	stat = {}     # dictionary - chars and number of repetitions
+	allchar = 0.0 # total number of characters
+	entropy = 0.0 # initial entropy
 
-allchar = 0.0
-entropy = 0.0
+	for line in sys.stdin.readlines():
+		line = re.sub(r'\s', '', line)
+		for znak in line:
+			if znak in stat:
+				stat[znak] += 1
+			else:
+				stat[znak] = 1
+			allchar += 1
 
-for line in sys.stdin.readlines():
-	line = re.sub(r'\s', '', line)
-	for znak in line:
-		if znak in stat:
-			stat[znak] += 1
-		else:
-			stat[znak] = 1
-		allchar += 1
+	for znak in stat:
+		stat[znak] = stat[znak]/allchar
+		entropy += stat[znak] * log(stat[znak], 2)
 
-for znak in stat:
-	stat[znak] = stat[znak]/allchar
-	entropy += stat[znak] * log(stat[znak], 2)
+	entropy *= -1
+	return entropy
 
-entropy *= -1
-
-print "Entropy == %g" % entropy
+if __name__ == "__main__":
+	print "Entropy == %g" % calculate_entropy()
