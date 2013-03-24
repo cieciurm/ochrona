20e10ec169307a8ec787fa054b2e8aa50e68a586
Mateusz Cieciura <mateusz5000@gmail.com>
2013-03-24 18:45:15 +0100

    Xorowanie liczb i stringów

--- lab5/xor.py
+++ lab5/xor.py
@@ -1,23 +1,26 @@
-from operator import xor
-from sys import *
+import sys
 
-a = int(argv[1])
-b = int(argv[2])
+def xor_numbers(a, b):
+	"""XORs two decimal numbers and produces a result as a decimal aswell"""
+	bin_a = bin(a)
+	bin_b = bin(b)
+	#max_len = 0;
+	#if len(bin_a) >= len(bin_b):
+		#max_len = len(bin_a)
+	#else:
+		#max_len = len(bin_b)
+	#print bin(a).zfill(max_len),"   ==",a
+	#print bin(b).zfill(max_len),"   ==",b
+	#print "(XOR)-----------------------"
+	result = a^b
+	#print bin(result).zfill(max_len),"   ==",result
+	return result
 
-bin_a = bin(a)
-bin_b = bin(b)
+def xor_string(a, b):
+	"""XORs two string produces a result as a string aswell"""
+	v = [chr(ord(a) ^ ord(b)) for a, b in zip(a, b)]
+	return ''.join(v).encode("hex")
 
-max_len = 0;
-
-if len(bin_a) >= len(bin_b):
-	max_len = len(bin_a)
-else:
-	max_len = len(bin_b)
-
-print bin(a).zfill(max_len),"   ==",a
-print bin(b).zfill(max_len),"   ==",b
-
-print "(XOR)-----------------------"
-
-result = a^b
-print bin(result).zfill(max_len),"   ==",result
+if __name__ == "__main__":
+	print xor_numbers(65, 98)
+	print xor_string(sys.argv[1], sys.argv[2])
